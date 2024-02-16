2302b5e755778ada65ba1e776041fddbc3e6e3d4
Julian L <julian.lawrance@gmail.com>
2024-02-16 10:26:16 +0000

    Consume a user location data service implementation in UserLocationController

--- AirboxTechnical/Controllers/UserLocationController.cs
+++ AirboxTechnical/Controllers/UserLocationController.cs
@@ -1,4 +1,5 @@
 using AirboxTechnical.Models;
+using AirboxTechnical.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AirboxTechnical.Controllers
@@ -7,23 +8,21 @@ namespace AirboxTechnical.Controllers
     [Route("[controller]")]
     public class UserLocationController : ControllerBase
     {
+        private readonly IUserLocationService _userLocationService;
         private readonly ILogger<UserLocationController> _logger;
 
-        public UserLocationController(ILogger<UserLocationController> logger)
+        public UserLocationController(
+            IUserLocationService userLocationService, 
+            ILogger<UserLocationController> logger)
         {
+            _userLocationService = userLocationService;
             _logger = logger;
         }
 
         [HttpGet(Name = "GetLastLocation")]
-        public Task<UserLocation> GetLastLocation(string userId)
+        public async Task<UserLocation> GetLastLocation(string userId)
         {
-            return Task.FromResult(new UserLocation
-            {
-                User = new User
-                {
-                    Id = userId,
-                },
-            });
+            return await _userLocationService.GetLastLocation(userId);
         }
     }
 }
