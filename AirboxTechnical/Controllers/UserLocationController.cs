dc68e4124f88e67e889226639fb64f4a77c57b15
Julian L <julian.lawrance@gmail.com>
2024-02-16 10:29:56 +0000

    Vacuously implement remaining UserLocationController methods

--- AirboxTechnical/Controllers/UserLocationController.cs
+++ AirboxTechnical/Controllers/UserLocationController.cs
@@ -12,17 +12,43 @@ namespace AirboxTechnical.Controllers
         private readonly ILogger<UserLocationController> _logger;
 
         public UserLocationController(
-            IUserLocationService userLocationService, 
+            IUserLocationService userLocationService,
             ILogger<UserLocationController> logger)
         {
             _userLocationService = userLocationService;
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetLastLocation")]
+        [HttpPost(Name = "AddLocation")]
+        public async Task<UserLocation> AddLocation(UserLocation location)
+        {
+            _logger.LogInformation($"Adding location for user {location.User.Id}");
+
+            return await _userLocationService.AddLocation(location);
+        }
+
+        [HttpGet("recent/{userId}", Name = "GetLastLocation")]
         public async Task<UserLocation> GetLastLocation(string userId)
         {
+            _logger.LogInformation($"Getting last location for user {userId}");
+
             return await _userLocationService.GetLastLocation(userId);
+        }
+
+        [HttpGet("recent", Name = "GetLastLocations")]
+        public async Task<IEnumerable<UserLocation>> GetLastLocations()
+        {
+            _logger.LogInformation("Getting last locations for all users");
+
+            return await _userLocationService.GetLastLocations();
+        }
+
+        [HttpGet(Name = "GetLocations")]
+        public async Task<IEnumerable<UserLocation>> GetLocations(string userId)
+        {
+            _logger.LogInformation($"Getting all locations for user {userId}");
+
+            return await _userLocationService.GetLocations(userId);
         }
     }
 }
