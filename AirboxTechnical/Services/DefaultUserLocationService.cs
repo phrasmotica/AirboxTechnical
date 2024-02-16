505b3982265c214dd17e700cdb9c9b556fcfeae0
Julian L <julian.lawrance@gmail.com>
2024-02-16 10:26:01 +0000

    Create interface and default implementation of user location data service

---
+++ AirboxTechnical/Services/DefaultUserLocationService.cs
@@ -0,0 +1,33 @@
+﻿using AirboxTechnical.Models;
+
+namespace AirboxTechnical.Services
+{
+    public class DefaultUserLocationService : IUserLocationService
+    {
+        public Task<UserLocation> AddLocation(UserLocation location)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserLocation> GetLastLocation(string userId)
+        {
+            return Task.FromResult(new UserLocation
+            {
+                User = new User
+                {
+                    Id = userId,
+                },
+            });
+        }
+
+        public Task<IEnumerable<UserLocation>> GetLastLocations()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<UserLocation>> GetLocations(string userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AirboxTechnical/Services/IUserLocationService.cs
@@ -0,0 +1,15 @@
+﻿using AirboxTechnical.Models;
+
+namespace AirboxTechnical.Services
+{
+    public interface IUserLocationService
+    {
+        Task<UserLocation> AddLocation(UserLocation location);
+
+        Task<UserLocation> GetLastLocation(string userId);
+
+        Task<IEnumerable<UserLocation>> GetLastLocations();
+
+        Task<IEnumerable<UserLocation>> GetLocations(string userId);
+    }
+}
