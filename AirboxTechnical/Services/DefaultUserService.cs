12093ce33b72aca9ec6ce8e13aa9e77907a535d8
Julian L <julian.lawrance@gmail.com>
2024-02-16 11:32:43 +0000

    Create interface and default implementation of data service for user records

---
+++ AirboxTechnical/Services/DefaultUserService.cs
@@ -0,0 +1,21 @@
+﻿using AirboxTechnical.Models;
+
+namespace AirboxTechnical.Services
+{
+    public class DefaultUserService : IUserService
+    {
+        private readonly List<User> _users = [];
+
+        public Task<User> AddUser(User user)
+        {
+            _users.Add(user);
+
+            return Task.FromResult(user);
+        }
+
+        public Task<User?> GetUser(string id)
+        {
+            return Task.FromResult(_users.SingleOrDefault(u => string.Equals(u.Id, id)));
+        }
+    }
+}
---
+++ AirboxTechnical/Services/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using AirboxTechnical.Models;
+
+namespace AirboxTechnical.Services
+{
+    public interface IUserService
+    {
+        Task<User> AddUser(User user);
+
+        Task<User?> GetUser(string id);
+    }
+}
